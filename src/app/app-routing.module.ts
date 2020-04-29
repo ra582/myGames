d3ee4c15e7533b546da4a850de1ede3acb50bd36
Rafael <rafaelsantos134500@gmail.com>
2020-04-28 21:26:43 -0300

    Att

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,14 +6,20 @@ import { NewComponent } from './new/new.component';
 import { E404Component } from './e404/e404.component';
 import { AboutComponent } from './about/about.component';
 
-
 const routes: Routes = [
-  {path: '', redirectTo: 'home', pathMatch: 'full'},
-  {path: 'home', component: HomeComponent},
-  {path: 'list', component: ListComponent, data: { title: 'Lista de Jogos'} },
-  {path: 'new', component: NewComponent, data: { title: 'Novo Jogo'} },
- {path: 'about', component: AboutComponent, data: {title: 'Sobre o My Games'}},
-  {path: '**', component: E404Component, data: {title: 'Página não encontrada'}}
+
+  // Rota não especificada é redirecionada para a "home"
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+
+  // Rota para a listagem dos games
+  { path: 'list', component: ListComponent, data: { title: 'Lista de Jogos' } },
+  
+  { path: 'new', component: NewComponent, data: { title: 'Novo Jogo' } },
+  { path: 'about', component: AboutComponent, data: { title: 'Sobre o My Games' } },
+
+  // Rota desconhecida é redirecionada para o componente "Erro 404"
+  { path: '**', component: E404Component, data: { title: 'Página não encontrada' } }
 ];
 
 @NgModule({
--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { AngularFirestoreCollection } from '@angular/fire/firestore/public_api';
+import { AngularFirestoreCollection, AngularFirestore } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
-import { AngularFirestore } from '@angular/fire/firestore';
 import { map } from 'rxjs/operators';
 
+// Modelagem dos dados
 export interface Game {
   cover: string;
   date: string;
@@ -13,9 +13,9 @@ export interface Game {
   title: string;
 }
 
-export interface GameId extends Game{
-id: string;
-
+// Modelagem com o Id do documento
+export interface GameId extends Game {
+  id: string;
 }
 
 @Component({
@@ -25,52 +25,91 @@ id: string;
 })
 export class ListComponent implements OnInit {
 
+  // Atributos
+
+  // Obtém a coleção do Firestore, conforme o modelo acima
   private gameCollection: AngularFirestoreCollection<Game>;
 
+  // Objeto games é tipo assíncrono que vai receber os dados do Db
   games: Observable<GameId[]>;
 
+  // Campo que será usado para ordenação dos dados
   orderBy: string;
 
+  // Direção da ordenação dos dados (ascedente ou descendente)
   orderDr: any;
-  constructor(private db: AngularFirestore) {
-   this.orderBy = "title";
 
-   this.orderDr="asc";
+  constructor(private db: AngularFirestore) {
+
+    // Ordena pelo título ao carregar
+    this.orderBy = 'title';
+
+    // Em ordem ascendente ao carregar
+    this.orderDr = 'asc';
+
   }
 
   ngOnInit(): void {
 
+    // Atualiza a lista de games
     this.getList();
+
   }
 
-  getList(){
-
+  getList() {
+    // Referência aos documentos da coleção 'games'
     this.gameCollection = this.db.collection<Game>('games', ref => ref.orderBy(this.orderBy, this.orderDr));
 
+    // Obtém os documento da coleção
     this.games = this.gameCollection.snapshotChanges().pipe(
-    map(actions => actions.map(a => {
-   const data = a.payload.doc.data() as Game;
-   const id = a.payload.doc.id;
+      map(actions => actions.map(a => {
+        const data = a.payload.doc.data() as Game;
+        const id = a.payload.doc.id;
 
-   return {id, ...data};
-    }))
+        // Retorna os documentos quando eles forem obtidos
+        return { id, ...data };
+      }))
     );
   }
+
+  // Altera o campo usado para ordenar a listagem
   changeOrderField(field: string) {
- if(this.orderBy !== field){
-   this.orderBy = field;
-   this.getList();
- }
- return false;
+
+    if (this.orderBy !== field) { // Se o campo selecionado é diferente do atual
+      this.orderBy = field; // Muda o valor do campo
+      this.getList(); // Atualizar a listagem com o novo parâmetro
+    }
+    return false; // Sai sem fazer nada
+
   }
 
   changeOrderDir(direction: any) {
-    if(this.orderDr !== direction){
-      this.orderDr = direction;
-      this.getList();
+
+    if (this.orderDr !== direction) { // Se o campo selecionado é diferente do atual
+      this.orderDr = direction; // Muda o valor do campo
+      this.getList(); // Atualizar a listagem com o novo parâmetro
     }
-    return false;
-     }
+    return false; // Sai sem fazer nada
 
   }
 
+  // Apaga um documento
+  deleteGame(gameKey, gameTitle) {
+
+    // Mensagem para confirmar se deseja apagar (Observe o ! = NOT)
+    if (!confirm(`Oooops!\nTem certeza que deseja apagar "${gameTitle}" da sua coleção?`)) {
+      return false;
+    }
+
+    this.db.collection('games').doc(gameKey).delete()
+      .then(res => {
+        alert(`"${gameTitle}" foi apagado da sua coleção!\nClique em [Ok] para continuar.`);
+      })
+      .catch(err => {
+        console.error(`Falha ao apagar: ${err}`);
+      });
+
+    return false;
+
+  }
+}
